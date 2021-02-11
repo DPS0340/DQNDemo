21722fd85e84d26db47f94dcd3b85c7bf3a9d25a
jiho lee <optional.int@kakao.com>
2021-02-11 16:47:57 +0900

    prototype

--- agent_1.py
+++ agent_1.py
@@ -23,21 +23,20 @@ def get_demo_traj():
 class DQfDNetwork(nn.Module):
     def __init__(self, in_size, out_size):
         super(DQfDNetwork, self).__init__()
-        HIDDEN_SIZE = 128
+        HIDDEN_SIZE = 256
         self.f1 = nn.Linear(in_size, HIDDEN_SIZE)
         self.f2 = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
         self.f3 = nn.Linear(HIDDEN_SIZE, out_size)
         nn.init.xavier_uniform_(self.f1.weight)
         nn.init.xavier_uniform_(self.f2.weight)
-        nn.init.xavier_uniform_(self.f3.weight)
         self.opt = torch.optim.Adam(self.parameters(), lr=0.001)
-        self.loss = torch.nn.MSELoss()
+        self.loss = torch.nn.MSELoss(reduction='sum')
 
     def forward(self,x):
-        x1 = self.f1(x)
+        x1 = F.relu(self.f1(x))
         x2 = self.f2(x1)
-        x3 = torch.sigmoid(self.f3(x2))
-        res = torch.tensor([(x3 >= 0.5).int()])
+        x3 = self.f3(x2)
+        res = F.softmax(x3)
         return res
 
 ##########################################################################
@@ -58,37 +57,43 @@ class DQfDAgent(object):
         self.env = env
         self.use_per = use_per
         self.gamma = 0.99
-        self.epsilon = 0.95
+        self.epsilon = 0.98
         self.low_epsilon = 0.01
-        self.policy_network = DQfDNetwork(4, 1)
-        self.target_network = DQfDNetwork(4, 1)
-        self.target_network.load_state_dict(self.policy_network.state_dict())
-        self.target_network.eval()
-        self.frequency = 50
+        self.policy_network = DQfDNetwork(4, 2)
+        self.target_network = DQfDNetwork(4, 2)
+        self.frequency = 1
     
     def get_action(self, state):
         # epsilon-greedy 적용 #
         randint = random.random()
-        if randint > self.epsilon:
-            return torch.tensor([random.randint(0, 1)])
+        if randint >= self.epsilon:
+            return random.randint(0, 1)
         else:
-            return self.target_network.forward(state)
+            predicted = self.policy_network.forward(state)
+            m = torch.distributions.Categorical(predicted)
+            action = m.sample()
+            action = action.numpy()
+            return action
     
 
-    def train_network(self):
+    def train_network(self, args=None, pretrain=False):
         # 람다값 임의로 설정 #
-        l1 = l2 = l3 = 0.3
+        l1 = l2 = l3 = 0.15
         # n은 논문에 나온대로 10 설정 #
-        self.n = 50
 
-        minibatch = self.sample_minibatch()
+        if pretrain:
+            self.n = 10
+            minibatch = self.sample_minibatch()
+        else:
+            self.n = 1
+            minibatch = [args]
 
         for episode in range(self.n):
-            state, action, reward, next_state, _ = minibatch[episode]
+            state, action, reward, next_state, done = minibatch[episode]
             state = torch.from_numpy(state).float()
             next_state = torch.from_numpy(next_state).float()
             # double_dqn_loss 계산 # 
-            double_dqn_loss = torch.Tensor([(reward + self.gamma * self.target_network.forward(next_state) - self.target_network(state)) ** 2])
+            double_dqn_loss = torch.Tensor([(reward + self.gamma * self.get_action(next_state) - self.get_action(state)) ** 2])
             double_dqn_loss.requires_grad = True
             def margin(action1, action2):
                 if action1 == action2:
@@ -99,30 +104,30 @@ class DQfDAgent(object):
             for selected_action in range(2):
                 __state__, _, _, _ = self.env.step(selected_action)
                 __state__ = torch.from_numpy(__state__).float()
-                partial_margin_classification_loss = max(partial_margin_classification_loss, self.target_network.forward(__state__) + margin(action, selected_action))
-            margin_classification_loss = partial_margin_classification_loss - self.target_network.forward(state)
+                partial_margin_classification_loss = max(partial_margin_classification_loss, self.get_action(__state__) + margin(action, selected_action))
+            margin_classification_loss = partial_margin_classification_loss - self.get_action(state)
             # n-step returns 계산 #
             n_step_returns = torch.Tensor([reward])
             current_n_step_action = action
-            current_n_step_state, current_reward, done, _ = self.env.step(current_n_step_action)
+            current_n_step_state, current_reward, __done__, _ = self.env.step(current_n_step_action)
             for exp in range(1, self.n):
-                if done:
+                if __done__:
                     break
                 current_n_step_state = torch.from_numpy(current_n_step_state).float()
-                current_n_step_action = self.target_network.forward(current_n_step_state)
-                current_n_step_state, current_reward, done, _ = self.env.step(action)
+                current_n_step_action = self.get_action(current_n_step_state)
+                current_n_step_state, current_reward, __done__, _ = self.env.step(action)
                 n_step_returns += (self.gamma ** exp) * current_reward
             partial_n_step_returns = -99999
             for selected_action in range(2):
                 __state__, _, _, _ = self.env.step(selected_action)
                 __state__ = torch.from_numpy(__state__).float()
-                partial_n_step_returns = max(partial_n_step_returns, self.target_network.forward(__state__) + margin(action, selected_action))
+                partial_n_step_returns = max(partial_n_step_returns, self.get_action(__state__) + margin(action, selected_action))
             n_step_returns + partial_n_step_returns
-            # loss 계산 #
-            # L2 정규화는 MSE로 대체 #
-            loss = double_dqn_loss + l1 * margin_classification_loss + l2 * n_step_returns + torch.Tensor([l3 * self.policy_network.loss(state, next_state)])
             # 오차역전파로 기울기 함수 학습 #
             self.policy_network.opt.zero_grad()
+            # loss 계산 #
+            # L2 정규화는 MSE로 대체 #
+            loss = double_dqn_loss + l1 * margin_classification_loss + l2 * n_step_returns + torch.Tensor([l3 * self.target_network.loss(state, next_state)])
             loss.backward()
             self.policy_network.opt.step()
 
@@ -133,17 +138,16 @@ class DQfDAgent(object):
         else:
             result = []
             for _ in range(self.n):
-                choice = random.randint(0, 1)
+                choice = random.randint(0, self.d_replay.shape[0]-1)
                 choice = self.d_replay[choice]
                 choice = random.choice(choice)
                 result.append(choice)
             return result
 
     def pretrain(self):
-        for i in range(5):
+        for i in range(100):
             print(f"{i} pretrain step")
-            self.env.reset()
-            self.train_network()
+            self.train_network(pretrain=True)
             if i % self.frequency == 0:
                 self.target_network.load_state_dict(self.policy_network.state_dict())
 
@@ -156,14 +160,16 @@ class DQfDAgent(object):
         test_min_episode = np.inf
         ###### 1. DO NOT MODIFY FOR TESTING ######
         env = self.env
+        env.reset()
+        env.render()
         dqfd_agent = self
         self.d_replay = get_demo_traj()
-        buffer = []
         self.td_errors = []
         # Do pretrain
         self.pretrain()
         ## TODO
 
+        buffer = []
 
         for e in range(self.n_EPISODES):
             ########### 2. DO NOT MODIFY FOR TESTING ###########
@@ -174,22 +180,24 @@ class DQfDAgent(object):
             done = False
             state = env.reset()
             state = torch.from_numpy(state).float()
+            env.render()
 
             while not done:
+                env.render()
                 ## TODO
-                action = dqfd_agent.get_action(state).numpy()[0]
-
+                action = dqfd_agent.get_action(state)
                 ## TODO
 
                 next_state, reward, done, _ = env.step(action)
                 next_state = torch.from_numpy(next_state).float()
-                buffer.append([state.numpy(), action, reward, next_state.numpy(), done])
+                to_append = [state.numpy(), action, reward, next_state.numpy(), done]
+                buffer.append(np.array(to_append))
                 ########### 3. DO NOT MODIFY FOR TESTING ###########
                 test_episode_reward += reward      
                 ########### 3. DO NOT MODIFY FOR TESTING  ###########
 
                 ## TODO
-                self.train_network()
+                self.train_network(np.array(to_append))
                 ########### 4. DO NOT MODIFY FOR TESTING  ###########
                 if done:
                     test_mean_episode_reward.append(test_episode_reward)
@@ -199,7 +207,9 @@ class DQfDAgent(object):
                 ########### 4. DO NOT MODIFY FOR TESTING  ###########
                 state = next_state
                 ## TODO
-            self.d_replay = np.append(self.d_replay, np.array(buffer))
+            self.d_replay = self.d_replay.tolist()
+            self.d_replay.append(buffer)
+            self.d_replay = np.array(self.d_replay)
             buffer = []
             ########### 5. DO NOT MODIFY FOR TESTING  ###########
             if test_over_reward:
